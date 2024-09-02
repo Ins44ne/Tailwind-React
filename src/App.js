301853fcff62d9a0c203584b3255e5a6343970f0
Ins44ne <ins44ne91@gmail.com>
2024-09-02 10:34:45 +0300

    feat: add components to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react'
+import { Wrapper } from './components/wrapper/'
+import { Header } from './components/header/'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Wrapper>
+      <Header></Header>
+    </Wrapper>
   )
 }
 
